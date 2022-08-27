d9711f3e9a32937779f1b504f9506adb44481948
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-26 18:20:38 -0600

    user story quatro

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/cycle_shops', to: 'cycle_shops#index' 
   get 'cycle_shops/:id', to: 'cycle_shops#show'
   get '/bikes', to: 'bikes#index'
+  get 'bikes/:id', to: 'bikes#show'
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/features/bike/show_spec.rb
+++ spec/features/bike/show_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
+# User Story 4, Child Show 
 
+# As a visitor
+# When I visit '/child_table_name/:id'
+# Then I see the child with that id including the child's attributes:
 RSpec.describe Bike, type: :feature do
-  describe 'As a visitor when I visit /child_table_name' do
-    it 'Then I see each Child in the system including the Child\'s attributes' do
+  describe 'As a visitor when I visit /child_table_name/:id' do
+    it 'Then I see the child with that id including the child\'s attributes:' do
       shop1 = CycleShop.create!({name: "Wheat Ridge Cyclery", rental_program: true, bike_capacity: 98})
       bike1 = Bike.create!({brand: "Revel", frame_size: 19, demo_available: true, cycle_shop_id: shop1.id})
       bike2 = Bike.create!({brand: "Why", frame_size: 17, demo_available: false, cycle_shop_id: shop1.id})   
       
-      visit "/bikes"
+      visit "/bikes/#{bike1.id}"
       
       expect(page).to have_content(bike1.brand)
       expect(page).to have_content(bike1.frame_size)
       expect(page).to have_content(bike1.demo_available)
-      expect(page).to have_content(bike2.brand)
-      expect(page).to have_content(bike2.frame_size)
-      expect(page).to have_content(bike2.demo_available)
     end
   end
 end
