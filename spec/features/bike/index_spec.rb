52ff079ebeb4bcc354563edccb9be8d4c1296f8e
Thomas Hayes <hayes.thomas610@gmail.com>
2022-08-28 15:40:09 -0600

    user story diecisiete

--- spec/features/bike/index_spec.rb
+++ spec/features/bike/index_spec.rb
@@ -44,5 +44,18 @@ RSpec.describe Bike, type: :feature do
       expect(page).to_not have_content('Seven')
       expect(page).to have_content('Commencal')
     end
+
+    it 'can update child from the childs index page' do
+      shop1 = CycleShop.create!({name: "Wheat Ridge Cyclery", rental_program: true, bike_capacity: 98})
+      bike1 = Bike.create!({brand: "Revel", frame_size: 19, demo_available: true, cycle_shop_id: shop1.id})
+      bike2 = Bike.create!({brand: "Why", frame_size: 17, demo_available: false, cycle_shop_id: shop1.id})
+      bike3 = Bike.create!({brand: "Seven", frame_size: 17, demo_available: false, cycle_shop_id: shop1.id})
+      bike4 = Bike.create!({brand: "Commencal", frame_size: 15, demo_available: true, cycle_shop_id: shop1.id})
+      
+      visit "/bikes"
+
+      
+
+    end
   end
 end
